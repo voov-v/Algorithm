5dda5e5637cda2392b8156bbd6ce21c3827a2d13
voov-v <yumin5482@gmail.com>
2025-12-24 09:15:53 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = -1;
+    if (-50000 <= num1 <= 50000 && -50000 <= num2 <=50000 ){
+        answer = num1 + num2;
+    }
+    return answer;
+}
