cb43bc0034a0437a4cfa36d10b82639ec24c1aba
voov-v <yumin5482@gmail.com>
2025-12-26 09:38:04 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    if (0 < n && n <= 1000){
+        for (int i = 1; i <= n; i++){
+            if (i % 2 == 0){
+                answer += i;
+            }
+        }
+    }
+    
+    return answer;
+}
