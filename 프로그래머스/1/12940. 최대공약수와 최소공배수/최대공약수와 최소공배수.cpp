713ecb88b652f0ed083c9570d1a882171c61814e
voov-v <yumin5482@gmail.com>
2026-02-19 09:49:26 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    int a = (n > m) ? n : m;
+    int b = (n > m) ? m : n;
+    
+    while(b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    
+    int gcd = a;
+    
+    answer.push_back(gcd);
+    answer.push_back((n / gcd) * m);
+    
+    return answer;
+}
