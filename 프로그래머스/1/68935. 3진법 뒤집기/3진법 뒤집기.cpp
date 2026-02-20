86ee8fa2eef567333cb241bc82802c05a9755234
voov-v <yumin5482@gmail.com>
2026-02-20 09:51:04 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    std::vector<int> vec;
+    while (n)
+    {
+        vec.push_back(n % 3);
+        n /= 3;
+    }
+
+    int power = 1;
+    for (int i = vec.size() - 1; i >= 0; --i)
+    {
+        answer += vec[i] * power;
+        power *= 3;
+    }
+
+    return answer;
+}
